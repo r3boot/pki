9a1889305127eff1a310d91897a9beb1d5e9cce3
Lex van Roon <r3boot@r3blog.nl>
2015-07-16 12:55:39 +0200

    moved/renamed valid_srcip to owns_fqdn in server checks

--- pkilib/server/checks.py
+++ pkilib/server/checks.py
@@ -7,8 +7,13 @@
 """
 
 import re
+import socket
 
 import pkilib.log as log
+
+
+# Global flag indicting permissive mode
+PERMISSIVE_MODE = False
 
 
 def valid_fqdn(fqdn=None):
@@ -34,3 +39,53 @@ def valid_fqdn(fqdn=None):
     parser = re.compile(regexp)
     result = parser.search(fqdn)
     return result is not None
+
+
+def owns_fqdn(srcip=None, fqdn=None):
+    """Check if a fqdn is owned by srcip. It does this by performing a DNS
+    lookup for the PTR records of fqdn, and matches srcip against these. If
+    a match is found, True is returned, else False. Note that the ip check
+    can be overridden by using PERMISSIVE_MODE. This function will also
+    return False if srcip and/or fqdn are invalid.
+
+    :param srcip:   Source ip address to check against
+    :type  srcip:   str
+    :param fqdn:    Fully-Qualified Domain-Name to check
+    :type  fqdn:    str
+    :returns:       True if fqdn is owned by srcip, else False
+    :rtype:         bool
+    """
+    if not srcip:
+        log.warning('srcip cannot be None')
+        return False
+    if not isinstance(srcip, str):
+        log.warning('srcip need to be a string')
+        return False
+    if not valid_fqdn(fqdn):
+        return False
+
+    # Get the PTR entries for fqdn
+    try:
+        socket_data = socket.getaddrinfo(fqdn, 80)
+    except socket.gaierror as err:
+        log.warning('Failed to resolve PTR for {0}: {1}'.format(fqdn, err))
+        return False
+
+    # Parse socket_data and assemble a list of ip addresses for fqdn
+    ips = []
+    for item in socket_data:
+        ipaddr = item[4][0]
+        if ipaddr not in ips:
+            ips.append(ipaddr)
+
+    # Check if srcip is one of the ip addresses of fqdn. Return True if
+    # permissive mode is enabled
+    if srcip not in ips:
+        if not PERMISSIVE_MODE:
+            log.warning('{0} does not belong to {1}'.format(fqdn, srcip))
+            return False
+        else:
+            log.warning('{0} does not belong to {1} (permissive)'.format(
+                fqdn, srcip
+            ))
+    return True
--- pkilib/server/tests/checks.py
+++ pkilib/server/tests/checks.py
@@ -2,6 +2,9 @@ import nose
 
 import pkilib.server.checks as checks
 
+LOCALHOST_A = 'localhost'
+LOCALHOST_PTR = '127.0.0.1'
+
 
 class test_valid_fqdn:
     def test_integer_fqdn(self):
@@ -33,3 +36,41 @@ class test_valid_fqdn:
 
     def test_fqdn_3component(self):
         assert checks.valid_fqdn('some.host.name') is True
+
+
+class test_owns_fqdn:
+    def test_undefined_srcip(self):
+        assert checks.owns_fqdn(None, LOCALHOST_A) is False
+
+    def test_empty_srcip(self):
+        assert checks.owns_fqdn('', LOCALHOST_A) is False
+
+    def test_numeric_srcip(self):
+        assert checks.owns_fqdn(12345, LOCALHOST_A) is False
+
+    def test_undefined_fqdn(self):
+        assert checks.owns_fqdn(LOCALHOST_PTR, None) is False
+
+    def test_empty_fqdn(self):
+        assert checks.owns_fqdn(LOCALHOST_PTR, '') is False
+
+    def test_numeric_fqdn(self):
+        assert checks.owns_fqdn(LOCALHOST_PTR, 12345) is False
+
+    def test_unowned_fqdn(self):
+        assert checks.owns_fqdn(LOCALHOST_PTR, 'example.com') is False
+
+    def test_invalid_fqdn(self):
+        assert checks.owns_fqdn(LOCALHOST_PTR, 'some_host_name') is False
+
+    def test_nonexisting_fqdn(self):
+        assert checks.owns_fqdn(LOCALHOST_PTR, 'some.random.host') is False
+
+    def test_unowned_fqdn_permissive(self):
+        old_value = checks.PERMISSIVE_MODE
+        checks.PERMISSIVE_MODE = True
+        assert checks.owns_fqdn(LOCALHOST_PTR, 'example.com') is True
+        checks.PERMISSIVE_MODE = old_value
+
+    def test_owned_fqdn(self):
+        assert checks.owns_fqdn(LOCALHOST_PTR, LOCALHOST_A) is True
